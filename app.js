041bf3a0d1c10dac4f657c67cfb412d1fbe0cd2d
王天龙 <wangtianlong@wangtianlongdeMacBook-Air-2.local>
2026-04-11 00:37:23 +0800

    fix: remove bootstrap warmup call on launch

--- app.js
+++ app.js
@@ -1,11 +1,8 @@
-const store = require('./utils/store');
 const appConfig = require('./data/app-config');
 
 App({
   globalData: {
     brandName: appConfig.brandName
   },
-  onLaunch() {
-    store.ensureState().catch(() => {});
-  }
+  onLaunch() {}
 });
